e5821b8d28372f5edddb3621ddfb20b0e9a8b0e9
Konstantin Tsolev <Konstantin.Tsolev@fourth.com>
2017-02-15 17:45:47 +0200

    multiple select try

---
+++ app/routes/power-calendar.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(){
+    return [
+      'Wed Feb 08 2017 00:00:00 GMT+0200',
+      'Thu Feb 09 2017 00:00:00 GMT+0200',
+      'Fri Feb 10 2017 00:00:00 GMT+0200',
+      'Tue Feb 14 2017 00:00:00 GMT+0200',
+      'Thu Feb 16 2017 00:00:00 GMT+0200',
+      'Fri Feb 17 2017 00:00:00 GMT+0200',
+      'Sat Feb 18 2017 00:00:00 GMT+0200',
+      'Mon Feb 20 2017 00:00:00 GMT+0200',
+      'Tue Feb 21 2017 00:00:00 GMT+0200'    ]
+  }
+});
